b572f45e992ce33e193bdeecec4fc62a8140b69d
SNURFER <shido322@snu.ac.kr>
2022-03-18 23:25:43 +0900

    [study] 0318

---
+++ python/56.py
@@ -0,0 +1,20 @@
+from typing import List
+
+
+class Solution:
+    def merge(self, intervals: List[List[int]]) -> List[List[int]]:
+        merged = []
+
+        for interval in sorted(intervals, key=lambda x: x[0]):
+            if merged and merged[-1][1] >= interval[0]:
+                merged[-1][1] = max(merged[-1][1], interval[1])
+            else:
+                merged.append(interval)
+
+        return merged
+
+
+if __name__ == "__main__":
+    intervals = [[1, 3], [2, 6], [8, 10], [15, 18]]
+    solution = Solution()
+    solution.merge(intervals)
