3f99887a987cae3f13a4aa7e524f8408fff5e035
SNURFER <shido322@snu.ac.kr>
2022-05-07 21:41:45 +0900

    [study] 0507

---
+++ python/second/9.py
@@ -0,0 +1,8 @@
+class Solution:
+    def isPalindrome(self, x: int) -> bool:
+        return str(x) == str(x)[::-1]
+
+
+if __name__ == "__main__":
+    solution = Solution()
+    print(solution.isPalindrome(121))
