6d918165a340c67932a4418b32829d16aa9aa71c
SNURFER <shido322@snu.ac.kr>
2022-06-21 23:59:24 +0900

    [study] 0621

---
+++ python/third/215.py
@@ -0,0 +1,14 @@
+from typing import List
+
+
+class Solution:
+    def findKthLargest(self, nums: List[int], k: int) -> int:
+        nums.sort(key=lambda x:-x)
+        return nums[k]
+
+
+if __name__ == "__main__":
+    nums = [3, 2, 3, 1, 2, 4, 5, 5, 6]
+    k = 4
+    solution = Solution()
+    print(solution.findKthLargest(nums, k))
