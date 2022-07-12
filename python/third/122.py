40389f4cfee22156e1437d96324d8267c22c69f6
SNURFER <shido322@snu.ac.kr>
2022-07-12 23:45:15 +0900

    [study] 0712

---
+++ python/third/122.py
@@ -0,0 +1,17 @@
+from typing import List
+
+
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        profit = 0
+        for i in range(len(prices) - 1):
+            if prices[i] < prices[i + 1]:
+                profit += prices[i + 1] - prices[i]
+
+        return profit
+
+
+if __name__ == "__main__":
+    prices = [7, 1, 5, 3, 6, 4]
+    solution = Solution()
+    print(solution.maxProfit(prices))
