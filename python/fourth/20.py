7475a762b7f2d4bd9a438274ae084559ad2231bb
이시도 <isido@isidoui-MacBookPro.local>
2024-03-24 19:58:30 +0900

    [study] 0324

--- python/fourth/20.py
+++ python/fourth/20.py
@@ -1,7 +1,26 @@
 class Solution:
     def isValid(self, s: str) -> bool:
-        pass
+        ch_stack = []
+        pair_dic = {
+            '}': '{',
+            ']': '[',
+            ')': '(',
+        }
+        for ch in s:
+            if ch == '{' or ch == '[' or ch == '(':
+                ch_stack.append(ch)
+            else:
+                if len(ch_stack) == 0:
+                    return False
+                if pair_dic[ch] != ch_stack.pop():
+                    return False
+
+        if len(ch_stack) != 0:
+            return False
+
+        return True
 
 
 if __name__ == "__main__":
     solution = Solution()
+    print(solution.isValid("("))
