62bbfa94532abf24f9810b3ad379aa6f89c4bf29
SNURFER <shido322@snu.ac.kr>
2024-11-06 00:06:35 +0900

    [study] 1106

---
+++ python/grind75/second/67.py
@@ -0,0 +1,15 @@
+class Solution:
+    def addBinary(self, a: str, b: str) -> str:
+        a_num = int(a, 2)
+        b_num = int(b, 2)
+        return bin(a_num + b_num)[2:]
+
+
+
+
+if __name__ == "__main__":
+    a = "1010"
+    b = "1011"
+
+    solution = Solution()
+    print(solution.addBinary(a, b))
